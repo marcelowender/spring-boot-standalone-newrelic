718eab21257f4814fd25103ca631e6f3a07e5d2d
Marcelo Wender Couto Coelho <bi001553@bhz-a-001874.intermedium.local>
2019-09-19 17:49:30 -0300

    CreateProject

---
+++ src/main/java/br/com/springnewrelic/SpringNewrelicApplication.java
@@ -0,0 +1,13 @@
+package br.com.springnewrelic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewrelicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNewrelicApplication.class, args);
+	}
+
+}
